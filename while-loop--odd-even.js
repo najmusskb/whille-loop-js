c722ec0dfbe61527f1801b220da556f317c6cb3a
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-02 20:57:50 +0600

    first commit

---
+++ while-loop--odd-even.js
@@ -0,0 +1,34 @@
+var number = 1;
+
+while (number <= 10) {
+
+    console.log(number);
+    number++;
+}
+
+
+
+
+// odd  number 
+
+var NUmber = 1;
+while (NUmber <= 100) {
+
+    console.log(NUmber);
+    NUmber = NUmber + 2;
+
+}
+
+
+
+
+// even Number 
+var NUmber = 0;
+while (NUmber <= 100) {
+
+    console.log(NUmber);
+    NUmber = NUmber + 2;
+}
+
+
+
---
+++ while-loop.js
@@ -0,0 +1,29 @@
+
+// while loop kivabe kag kore  part-01
+
+
+// var = ____ ;  loop variable
+
+// while(condition){
+
+// }
+
+
+
+
+
+
+
+
+var roastGiven = 0;
+while (roastGiven < 7) {
+
+    console.log('roast den please');
+    roastGiven++;
+    console.log(roastGiven);
+
+    //  roastGiven = +1; roastGiven +=1; ei duita line samne roastGiven++; eitar motoi kag korbe .
+}
+
+
+
